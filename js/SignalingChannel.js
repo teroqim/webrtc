0bfdf237dfed3918d745959d0795caef4c9a69a8
Peter <omniboomer@hotmail.com>
2012-07-10 10:35:50 +0200

    Initial commit

---
+++ js/SignalingChannel.js
@@ -0,0 +1,82 @@
+/**
+ * Created with PyCharm.
+ * User: omniboomer
+ * Date: 2012-06-06
+ * Time: 14.24
+ * To change this template use File | Settings | File Templates.
+ */
+
+var onIncomingMessage = function(evt){console.log("Caling uninitialized onIncomingMessages");};
+
+var debugLog = false;
+
+var debug = function(str){
+    if(debugLog){
+        console.log(str);
+    }
+}
+
+function SignalingChannel(roomKey, userId, token){
+
+    this.roomKey = roomKey;
+    this.userId = userId;
+    this.token = token;
+
+    this.onChannelOpened = function() {
+        debug('Channel opened.');
+    }
+
+
+    this.onChannelMessage = function(message) {
+        debug('S->C: ' + message.data);
+        if (message.data != 'BYE') {
+            if (message.data.indexOf("\"ERROR\"", 0) == -1) {
+                onIncomingMessage(message);
+            }
+        } else {
+            debug('Session terminated.');
+        }
+    }
+
+    this.onChannelError = function() {
+        debug('Channel error.');
+    }
+
+    this.onChannelClosed = function() {
+        debug('Channel closed.');
+    }
+
+    this.close = function(){
+        debug("Closing socket");
+        this.socket.close();
+    };
+
+    //Handle incoming message
+    this.onIncomingMessage = function(func){
+        onIncomingMessage = func;
+    };
+
+    //Handle outgoing message
+    this.send = function(message){
+        debug('C->S: ' + message);
+        path = '/message?r=' + this.roomKey + '&u=' + this.userId;
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', path, true);
+        xhr.send(message);
+    };
+
+    //Initialize connection
+    this.initialize = function(){
+        debug("Opening channel.");
+        this.channel = new goog.appengine.Channel(this.token);
+        var handler = {
+            onopen: this.onChannelOpened,
+            onmessage: this.onChannelMessage,
+            onerror: this.onChannelError,
+            onclose: this.onChannelClosed
+        };
+
+        this.socket = this.channel.open(handler);
+    };
+
+}
